78cce383cddc08897cac5f6eed97538efa8378e2
marcuscastelo <marcuscastelo@usp.br>
2026-02-04 00:31:26 -0300

    feat: remove unused components and clean up app structure

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,6 @@
 import { Router } from '@solidjs/router'
 import { FileRoutes } from '@solidjs/start/router'
 import { Suspense } from 'solid-js'
-import { Nav } from '~/components/Nav'
 import './app.css'
 
 export default function App() {
@@ -10,7 +9,6 @@ export default function App() {
       root={(props) => (
         // The `root` div with class "root" enables isolation for BaseUI Portals (see app.css)
         <div class="root">
-          <Nav />
           <Suspense>{props.children}</Suspense>
         </div>
       )}
